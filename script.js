ae176c1a5f42a2cd951c2f5cf6e7d3aa13a26aef
Yuval Nahshony <200560745@student.georgianc.on.ca>
2025-06-20 13:49:20 -0400

    created full functionality for each of the buttons except random button

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const actList = document.getElementById("act");
 const adjList = document.getElementById("adj");
 const charList1 = document.getElementById("char1");
 const locList = document.getElementById("loc");
+const charBtn = document.getElementById("charbtn");
+const actBtn = document.getElementById("actbtn");
+const adjBtn = document.getElementById("adjbtn");
+const charBtn1 = document.getElementById("charbtn1");
+const locBtn = document.getElementById("locbtn");
+const randomBtn = document.getElementById("randombtn");
+const finalizeBtn = document.getElementById("finalizebtn");
 
 let chars = [
     "Astronaut",
@@ -141,3 +148,76 @@ locs.forEach((loc) => {
 locList.style.color="white";
 locList.style.backgroundColor="pink";
 locList.style.fontSize="20px";
+
+/* when clicking on buttons they do something. 
+cycle through  */
+
+let storyArray = [];
+let locationOfRowChar = 0;
+const charRows = charList.querySelectorAll("tr");
+charBtn.addEventListener('click', function() {
+    charRows.forEach(row => row.style.color = "white");
+    charRows.forEach(row => row.style.backgroundColor = "purple");
+    charRows[locationOfRowChar].style.color ="black";
+    charRows[locationOfRowChar].style.backgroundColor = "yellow";
+    storyElement = charRows[locationOfRowChar].textContent;
+    storyArray[0]= storyElement;
+    console.log(storyArray);
+    locationOfRowChar = (locationOfRowChar + 1) % charRows.length;
+});
+let locationOfRowAct = 0;
+const actRows = actList.querySelectorAll("tr");
+actBtn.addEventListener('click', function() {
+    actRows.forEach(row => row.style.color = "white");
+    actRows.forEach(row => row.style.backgroundColor = "blue");
+    actRows[locationOfRowAct].style.color ="black";
+    actRows[locationOfRowAct].style.backgroundColor = "yellow";
+    storyElement = actRows[locationOfRowAct].textContent;
+    storyArray[1]= storyElement;
+    console.log(storyArray);
+    locationOfRowAct = (locationOfRowAct + 1) % actRows.length;
+});
+let locationOfRowAdj = 0;
+const AdjRows = adjList.querySelectorAll("tr");
+adjBtn.addEventListener('click', function() {
+    AdjRows.forEach(row => row.style.color = "white");
+    AdjRows.forEach(row => row.style.backgroundColor = "green");
+    AdjRows[locationOfRowAdj].style.color ="black";
+    AdjRows[locationOfRowAdj].style.backgroundColor = "yellow";
+    storyElement = AdjRows[locationOfRowAdj].textContent;
+    storyArray[2]= storyElement;
+    console.log(storyArray);
+    locationOfRowAdj = (locationOfRowAdj + 1) % AdjRows.length;
+});
+let locationOfRowChar1 = 0;
+const char1Rows = charList1.querySelectorAll("tr");
+charBtn1.addEventListener('click', function() {
+    char1Rows.forEach(row => row.style.color = "white");
+    char1Rows.forEach(row => row.style.backgroundColor = "orange");
+    char1Rows[locationOfRowChar1].style.color ="black";
+    char1Rows[locationOfRowChar1].style.backgroundColor = "yellow";
+    storyElement = char1Rows[locationOfRowChar1].textContent;
+    storyArray[3]= storyElement;
+    console.log(storyArray);
+    locationOfRowChar1 = (locationOfRowChar1 + 1) % char1Rows.length;
+});
+let locationOfRowLoc = 0;
+const locRows = locList.querySelectorAll("tr");
+locBtn.addEventListener('click', function() {
+    locRows.forEach(row => row.style.color = "white");
+    locRows.forEach(row => row.style.backgroundColor = "pink");
+    locRows[locationOfRowLoc].style.color ="black";
+    locRows[locationOfRowLoc].style.backgroundColor = "yellow";
+    storyElement = locRows[locationOfRowLoc].textContent;
+    storyArray[4]= storyElement;
+    console.log(storyArray);
+    locationOfRowLoc = (locationOfRowLoc + 1) % locRows.length;
+});
+
+
+let output = document.getElementById("output");
+finalizeBtn.addEventListener('click', function() {
+    let stringStory = storyArray.join(" ");
+    output.textContent = stringStory;
+    console.log(stringStory);
+})
